884f306bf32613a773d36db27ff69ab1e6741a7c
gusLopezC <guslopezcallejas@gmail.com>
2020-08-13 23:36:30 -0500

    first commit

--- webHook.php
+++ webHook.php
@@ -6,14 +6,18 @@
     // Permite la ejecucion de los metodos
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
+  
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         
         //recibes los datos
         $pedido = json_decode(file_get_contents("php://input"),true);
-
+        $content    =   file_get_contents("php://input");
             //verificacion de la data enviada exista
+        echo($content);
         if (isset($pedido) && !empty($pedido)) {
  
+            
+
             header("HTTP/1.1 200 OK");
             //echo ''; respuesta
             exit();
