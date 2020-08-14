fd932bd71214e6496914c2f095f48e09d648a0b1
gusLopezC <guslopezcallejas@gmail.com>
2020-08-13 23:42:52 -0500

    first commit

--- webHook.php
+++ webHook.php
@@ -1,33 +1,33 @@
 <?php
 
-    // Permite la conexion desde cualquier origen
-    header("Access-Control-Allow-Origin: *");
-    
-    // Permite la ejecucion de los metodos
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+// Permite la conexion desde cualquier origen
+header("Access-Control-Allow-Origin: *");
 
-  
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
-        //recibes los datos
-        $pedido = json_decode(file_get_contents("php://input"),true);
-        $content    =   file_get_contents("php://input");
-            //verificacion de la data enviada exista
-        echo($content);
-        if (isset($pedido) && !empty($pedido)) {
- 
-            
+// Permite la ejecucion de los metodos
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-            header("HTTP/1.1 200 OK");
-            //echo ''; respuesta
-            exit();
 
-        }else{
-            header("HTTP/1.1 100 NO EXISTE DATA");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        }
+    //recibes los datos
+    $pedido = json_decode(file_get_contents("php://input"), true);
+    $content    =   file_get_contents("php://input");
+    //verificacion de la data enviada exista
+    // echo($content);
+    // if (isset($pedido) && !empty($pedido)) {
 
-    }
 
-    //En caso de que ninguna de las opciones anteriores se haya ejecutado
-    header("HTTP/1.1 400 Bad Request");
+
+    header("HTTP/1.1 200 OK");
+    //echo ''; respuesta
+    exit();
+
+    // }else{
+    //    header("HTTP/1.1 100 NO EXISTE DATA");
+
+    //}
+
+}
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
